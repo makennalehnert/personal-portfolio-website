a290e0c83e3e8ed2f1076cb819828ca88bd59632
Makenna Lehnert <makennaiklehnert@gmail.com>
2022-02-07 14:54:43 -0800

    made all links functional on homepage and did some styling

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import "./Home.css";
 
 export default function Home() {
     return (
         <div className="homemain">
             <h1>Home</h1>
-            <div>
-            <h4 className= "links">About</h4>
-            </div>
-            <div>
-            <h4 className= "links">Skills</h4>
-            </div>
-            <div>
-            <h4 className= "links">Education</h4>
-            </div>
-            <h4 className= "links"> Experience</h4>
-            <h4 className= "links">Projects</h4>
-            <h4 className= "links">Contact</h4>
+
+            <Link to="/about"><h4 className= "links">About</h4></Link>
+
+            <Link to="/skills"><h4 className= "links">Skills</h4></Link>
+
+            <Link to="/education"><h4 className= "links">Education</h4></Link>
+
+            <Link to="/experience"><h4 className= "links"> Experience</h4></Link>
+            
+            <Link to="/projects"><h4 className= "links">Projects</h4></Link>
+
+            <Link to="/contact"><h4 className= "links">Contact</h4></Link>
         </div>
     )
 }
